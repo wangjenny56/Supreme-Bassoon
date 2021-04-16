523a9ecc47fde5b8c27a35461b9372d9f350a39a
jwang4201 <60190662+jwang4201@users.noreply.github.com>
2021-04-16 19:50:30 -0400

    blueprint!

---
+++ OurBackend/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+// This just sends back a message for any URL path not covered above
+app.use('/', (req, res) => {
+	res.send('Default message.');
+    });
+
+// This starts the web server on port 3000. 
+app.listen(3000, () => {
+	console.log('Listening on port 3000');
+    });
