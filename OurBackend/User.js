79dcd049ae34989de3c40728addcbd783b05978c
wangjenny56 <20563230+wangjenny56@users.noreply.github.com>
2021-04-21 23:33:06 -0400

    defines User Schema

---
+++ OurBackend/User.js
@@ -0,0 +1,109 @@
+var mongoose = require('mongoose');
+
+// the host:port must match the location where you are running MongoDB
+// the "myDatabase" part can be anything you like
+const uri = "mongodb+srv://jwang6:EAC-_hFD4_K9+9a@cluster0.z6uhh.mongodb.net/test";
+
+//Set up default mongoose connection
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+  
+mongoose.connection.on('connected',() => {
+console.log('Mongoose is connected!');
+});
+
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+    username: String,
+	password: String,
+    restaurant_name: String, 
+    cuisine: String, 
+    customers_served: Number, 
+    email: String, 
+    phone_number: String, 
+    location: Object, 
+    hours: Object, 
+    listings: Array
+});
+
+/*var userSchema = new Schema({
+	username: String,
+	password: String,
+    restaurant_name: String, 
+    cuisine: String, 
+    customers_served: Number, 
+    email: String, 
+    phone_number: String, 
+    location: {
+        address: String, 
+        city: String, 
+        zipcode: String, 
+        state: String
+    }, 
+    hours: {
+        Monday: String, 
+        Tuesday: String, 
+        Wednesday: String, 
+        Thursday: String, 
+        Friday: String, 
+        Saturday: String, 
+        Sunday: String
+    }, 
+    listings: [
+        {
+            food_description: String,
+            food_type: String, 
+            quantity: Number, 
+            perishability: String, 
+            pick_up_time: String, 
+            availability_status: String, 
+            picked_up_by: String
+
+        }
+    ]
+}); */
+
+// export personSchema as a class called Person
+module.exports = mongoose.model('User', userSchema);
+
+/*
+{
+   "username" : "jwang3",
+   "password" : "password", 
+   "restaurant_name" : "Pho Street",
+   "cuisine" : "Vietnamese",
+   "customers_served" : "0",
+   "email" : "pho@gmail.com",
+   "phone_number" : "610-526-7440", 
+   "location" : {
+       "address" : "101 N Merion Ave",
+       "city" : "Bryn Mawr",
+       "zipcode" : "19010",
+       "state": "PA"
+   },
+   "hours": {
+       "Monday" : "8:00AM - 10:00PM",
+       "Tuesday" : "8:00AM - 10:00PM",
+       "Wednesday" : "8:00AM - 10:00PM",
+       "Thursday" : "8:00AM - 10:00PM",
+       "Friday" : "8:00AM - 10:00PM",
+       "Saturday" : "8:00AM - 12:00AM",
+       "Sunday" : "8:00AM - 12:00AM"
+   },
+   "listings" : [
+       {
+           "food_description" : "Beef Pho",
+           "food_type" : "Noodles",
+           "quantity" : "1",
+           "perishability" : "5 hours",
+           "pick_up_time" : "",
+           "availability_status" : "avaliable",
+           "picked_up_by" : ""
+       }
+   ]
+}
+
+*/
