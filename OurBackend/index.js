dad32116603af53e4cf4e1b024331651868d20a2
wangjenny56 <20563230+wangjenny56@users.noreply.github.com>
2021-04-21 23:31:30 -0400

    added mongoose, body parser, and endpoints

--- OurBackend/index.js
+++ OurBackend/index.js
@@ -1,12 +1,34 @@
+// set up Express
 var express = require('express');
 var app = express();
 
+// set up BodyParser
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// import the Person class from Person.js
+var User = require('./User.js');
+
+app.use('/all', (req, res) => {
+	User.find({}, (error, result) => {
+		if(error) {
+		return console.log(`Error has occurred: ${error}`);
+		}
+		res.send(result)
+	})
+});
+
 // This just sends back a message for any URL path not covered above
+app.use('/test', (req, res) => {
+	//return res.send('Default message.');  
+    res.json({ 'status' : 'It works!' })
+});
+
 app.use('/', (req, res) => {
 	res.send('Default message.');
-    });
+});
 
 // This starts the web server on port 3000. 
 app.listen(3000, () => {
 	console.log('Listening on port 3000');
-    });
+});
